c4f642ad6ac80c5eb30accb4894a8b2b87c52726
David Leibovych <ariedov@gmail.com>
2019-05-09 17:29:59 +0300

    tests for checking different share cases

--- src/test/kotlin/com/dleibovych/splitthetrip/DebtTest.kt
+++ src/test/kotlin/com/dleibovych/splitthetrip/DebtTest.kt
@@ -29,7 +29,7 @@ class DebtTest {
         val user1 = User(1, "first", 1, 900)
         val user2 = User(2, "second", 1, 0)
         val user3 = User(3, "third", 1, 0)
-        val shares = listOf(Share(user1, 300), Share(user2, -300), Share(user3, -300))
+        val shares = listOf(Share(user1, 600), Share(user2, -300), Share(user3, -300))
 
         val debt1 = calculateDebt(user1, shares)
         val debt2 = calculateDebt(user2, shares)
@@ -44,5 +44,27 @@ class DebtTest {
         assertEquals(1, debt3.size)
         assertEquals(user1, debt3[0].user)
         assertEquals(300, debt3[0].share)
+    }
+
+    @Test
+    fun testOneToTwo() {
+        val user1 = User(1, "first", 1, 450)
+        val user2 = User(2, "second", 1, 450)
+        val user3 = User(3, "third", 1, 0)
+
+        val shares = listOf(Share(user1, 150), Share(user2, 150), Share(user3, -300))
+
+        val debt1 = calculateDebt(user1, shares)
+        val debt2 = calculateDebt(user2, shares)
+        val debt3 = calculateDebt(user3, shares)
+
+        assertEquals(0, debt1.size)
+        assertEquals(0, debt2.size)
+
+        assertEquals(2, debt3.size)
+        assertEquals(user1, debt3[0].user)
+        assertEquals(user2, debt3[1].user)
+        assertEquals(150, debt3[0].share)
+        assertEquals(150, debt3[1].share)
     }
 }
--- src/test/kotlin/com/dleibovych/splitthetrip/ShareTest.kt
+++ src/test/kotlin/com/dleibovych/splitthetrip/ShareTest.kt
@@ -8,6 +8,48 @@ import org.junit.Test
 class ShareTest {
 
     @Test
+    fun testCalculationOneToOne() {
+        val share = calculateShare(
+            listOf(
+                User(1, "first", 1, 1000),
+                User(2, "second", 1, 0)
+            ),
+            2
+        )
+
+        assertEquals(2, share.size)
+
+        assertEquals(1, share[0].user.id)
+        assertEquals(500, share[0].share)
+
+        assertEquals(2, share[1].user.id)
+        assertEquals(-500, share[1].share)
+    }
+
+    @Test
+    fun testCalculationOneToOneWithThree() {
+        val share = calculateShare(
+            listOf(
+                User(1, "first", 1, 900),
+                User(2, "second", 1, 0),
+                User(3, "third", 1, 0)
+            ),
+            3
+        )
+
+        assertEquals(3, share.size)
+
+        assertEquals(1, share[0].user.id)
+        assertEquals(600, share[0].share)
+
+        assertEquals(2, share[1].user.id)
+        assertEquals(-300, share[1].share)
+
+        assertEquals(3, share[2].user.id)
+        assertEquals(-300, share[2].share)
+    }
+
+    @Test
     fun testCalculateShareWithTwoPayers() {
         val share = calculateShare(
             listOf(
