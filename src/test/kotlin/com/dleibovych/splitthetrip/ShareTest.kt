e54136a5b26732f0691ef12ef8dbc27d361f6fb2
David Leibovych <ariedov@gmail.com>
2019-05-09 16:04:24 +0300

    adds share test for three people

--- src/test/kotlin/com/dleibovych/splitthetrip/ShareTest.kt
+++ src/test/kotlin/com/dleibovych/splitthetrip/ShareTest.kt
@@ -25,4 +25,27 @@ class ShareTest {
         assertEquals(2, share[1].user.id)
         assertEquals(-4000, share[1].share)
     }
+
+    @Test
+    fun testCalculateShareWithThreePayers() {
+        val share = calculateShare(
+            listOf(
+                User(1, "first", 3, 1000),
+                User(2, "second", 7, 5000),
+                User(3, "third", 3, 7000)
+            ),
+            13
+        )
+
+        assertEquals(3, share.size)
+
+        assertEquals(1, share[0].user.id)
+        assertEquals(-2000, share[0].share)
+
+        assertEquals(2, share[1].user.id)
+        assertEquals(-2000, share[1].share)
+
+        assertEquals(3, share[2].user.id)
+        assertEquals(4000, share[2].share)
+    }
 }
