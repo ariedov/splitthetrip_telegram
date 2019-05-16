b27291c88e3a07aac67934a917205ab682d95389
David Leibovych <ariedov@gmail.com>
2019-05-16 15:50:24 +0300

    adds action router test

---
+++ src/test/kotlin/com/dleibovych/splitthetrip/ActionRouterTest.kt
@@ -0,0 +1,57 @@
+package com.dleibovych.splitthetrip
+
+import org.junit.Assert
+import org.junit.Test
+
+class ActionRouterTest {
+
+    @Test
+    fun testExpenseAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testConfirmExpenseAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testCurrencyAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testConfirmCurrencyAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testInfoAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testRegisterAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testConfirmRegisterAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testStartAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testTransactionAction() {
+        Assert.assertTrue(false)
+    }
+
+    @Test
+    fun testConfirmTransactionAction() {
+        Assert.assertTrue(false)
+    }
+}
