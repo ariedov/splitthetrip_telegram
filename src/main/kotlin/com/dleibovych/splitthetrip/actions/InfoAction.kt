ddb100987e37379f7fdbabac0a3aa0d480f862a5
David Leibovych <ariedov@gmail.com>
2019-05-17 14:47:54 +0300

    updates info action to behave correctly under harsh conditions

--- src/main/kotlin/com/dleibovych/splitthetrip/actions/InfoAction.kt
+++ src/main/kotlin/com/dleibovych/splitthetrip/actions/InfoAction.kt
@@ -3,6 +3,8 @@ package com.dleibovych.splitthetrip.actions
 import com.dleibovych.splitthetrip.calculator.calculateDebt
 import com.dleibovych.splitthetrip.calculator.calculateShare
 import com.dleibovych.splitthetrip.chatId
+import com.dleibovych.splitthetrip.data.Currency
+import com.dleibovych.splitthetrip.data.Expense
 import com.dleibovych.splitthetrip.data.Storage
 import me.ivmg.telegram.entities.Update
 import java.lang.StringBuilder
@@ -15,8 +17,33 @@ class InfoAction(private val storage: Storage) : Action {
         val expenses = storage.getExpenses()
         val transfers = storage.getTransfers()
 
-        val shares = calculateShare(users, currencies, expenses, transfers)
+        if (users.isEmpty()) {
+            messenger.sendMessage(
+                update.chatId!!,
+                text = "Немає зареєстрованих людей. Ви можете добавити себе через /register"
+            )
+            return
+        }
 
+        if (currencies.isEmpty()) {
+            messenger.sendMessage(
+                update.chatId!!,
+                text = "Немає жодної зареєстрованої валюти. Ви можете добавити одну через /addcurrency _назва_"
+            )
+            return
+        }
+
+        if (expenses.isEmpty()) {
+            messenger.sendMessage(1, text = "Немає зареєстрованих витрат. Ви можете добавити одну через /add _сума_")
+            return
+        }
+
+        if (users.size == 1) {
+            sendSingleUserMessage(update, currencies, expenses, messenger)
+            return
+        }
+
+        val shares = calculateShare(users, currencies, expenses, transfers)
         val result = users.fold(StringBuilder()) { rootBuilder, user ->
             val debts = calculateDebt(user, currencies, shares)
             debts.fold(rootBuilder, { builder, debt ->
@@ -34,5 +61,24 @@ class InfoAction(private val storage: Storage) : Action {
         }.toString().trim()
 
         messenger.sendMessage(update.chatId!!, text = result)
+    }
+
+    private fun sendSingleUserMessage(
+        update: Update,
+        currencies: List<Currency>,
+        expenses: List<Expense>,
+        messenger: TelegramMessenger
+    ) {
+        val message = currencies.fold(StringBuilder()) { builder, currency ->
+            expenses
+                .filter { it.currency == currency }
+                .fold(builder) { _, expense ->
+                    builder.append(expense.user.name)
+                    builder.append(" витратив ")
+                    builder.append("*${expense.amount} ${expense.currency.name}*")
+                }
+            builder.append("\n")
+        }.toString().trim()
+        messenger.sendMessage(update.chatId!!, text = message)
     }
 }
--- src/test/kotlin/com/dleibovych/splitthetrip/actions/InfoBuilderTest.kt
+++ src/test/kotlin/com/dleibovych/splitthetrip/actions/InfoBuilderTest.kt
@@ -41,6 +41,95 @@ class InfoBuilderTest {
         val message = createTelegramMessage(1, chat = createTelegramChat(1))
         action.perform(messenger, createTelegramUpdate(1, message = message))
 
-        verify(messenger).sendMessage(1, text = "user2 має повернути user1 - *300 usd*\nuser3 має повернути user1 - *300 usd*")
+        verify(messenger).sendMessage(
+            1,
+            text = "user2 має повернути user1 - *300 usd*\nuser3 має повернути user1 - *300 usd*"
+        )
+    }
+
+    @Test
+    fun testBuildNoUsers() {
+        whenever(storage.readUsers()).thenReturn(emptyList())
+        whenever(storage.getCurrencies()).thenReturn(listOf(Currency("usd")))
+        whenever(storage.getExpenses()).thenReturn(emptyList())
+        whenever(storage.getTransfers()).thenReturn(emptyList())
+
+        val message = createTelegramMessage(1, chat = createTelegramChat(1))
+        action.perform(messenger, createTelegramUpdate(1, message = message))
+
+        verify(messenger).sendMessage(1, text = "Немає зареєстрованих людей. Ви можете добавити себе через /register")
+    }
+
+    @Test
+    fun testBuildNoExpenses() {
+        val user1 = BotUser(1, "user1", 1)
+        val user2 = BotUser(2, "user2", 1)
+
+        whenever(storage.readUsers()).thenReturn(listOf(user1, user2))
+        whenever(storage.getCurrencies()).thenReturn(listOf(Currency("usd")))
+        whenever(storage.getExpenses()).thenReturn(emptyList())
+        whenever(storage.getTransfers()).thenReturn(emptyList())
+
+        val message = createTelegramMessage(1, chat = createTelegramChat(1))
+        action.perform(messenger, createTelegramUpdate(1, message = message))
+
+        verify(messenger).sendMessage(
+            1,
+            text = "Немає зареєстрованих витрат. Ви можете добавити одну через /add _сума_"
+        )
+    }
+
+    @Test
+    fun testBuildSingleUserOneCurrency() {
+        val user1 = BotUser(1, "user1", 1)
+
+        whenever(storage.readUsers()).thenReturn(listOf(user1))
+        whenever(storage.getCurrencies()).thenReturn(listOf(Currency("usd")))
+        whenever(storage.getExpenses()).thenReturn(listOf(Expense(user1, 900, Currency("usd"))))
+        whenever(storage.getTransfers()).thenReturn(emptyList())
+
+        val message = createTelegramMessage(1, chat = createTelegramChat(1))
+        action.perform(messenger, createTelegramUpdate(1, message = message))
+
+        verify(messenger).sendMessage(1, text = "user1 витратив *900 usd*")
+    }
+
+    @Test
+    fun testBuildSingleUserMultipleCurrencies() {
+        val user1 = BotUser(1, "user1", 1)
+
+        whenever(storage.readUsers()).thenReturn(listOf(user1))
+        whenever(storage.getCurrencies()).thenReturn(listOf(Currency("usd"), Currency("uah")))
+        whenever(storage.getExpenses()).thenReturn(
+            listOf(
+                Expense(user1, 900, Currency("usd")),
+                Expense(user1, 100, Currency("uah"))
+            )
+        )
+        whenever(storage.getTransfers()).thenReturn(emptyList())
+
+        val message = createTelegramMessage(1, chat = createTelegramChat(1))
+        action.perform(messenger, createTelegramUpdate(1, message = message))
+
+        verify(messenger).sendMessage(1, text = "user1 витратив *900 usd*\nuser1 витратив *100 uah*")
+    }
+
+
+    @Test
+    fun testBuildNoCurrencies() {
+        val user1 = BotUser(1, "user1", 1)
+
+        whenever(storage.readUsers()).thenReturn(listOf(user1))
+        whenever(storage.getCurrencies()).thenReturn(emptyList())
+        whenever(storage.getExpenses()).thenReturn(emptyList())
+        whenever(storage.getTransfers()).thenReturn(emptyList())
+
+        val message = createTelegramMessage(1, chat = createTelegramChat(1))
+        action.perform(messenger, createTelegramUpdate(1, message = message))
+
+        verify(messenger).sendMessage(
+            1,
+            text = "Немає жодної зареєстрованої валюти. Ви можете добавити одну через /addcurrency _назва_"
+        )
     }
 }
